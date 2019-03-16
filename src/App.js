c9cd8a7d66b1fb279b6ee0bc1af26a301b3e0294
hervekhg2 <hervekouamo@gmail.com>
2019-03-16 23:54:29 +0100

    7.Utilisation de State: Constante du composant

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+  	persons:[
+  		{ name : 'Herve', age:30 },
+  		{ name : 'Gael', age:20 },
+  		{ name : 'Isabelle', age:26 }
+  	]
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a react App for Learning </h1>
         <p>This working fine. I love it</p>
-        <button> Switch</button>
-        <Person name="Max" age="28"/>
-        <Person name="Gaël" age="30"> Je suis un vieux qui aime coder </Person>
-        <Person name="isabelle" age="26"/>
+        <button> Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
 
       </div>
     );
