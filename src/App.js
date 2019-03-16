e430b9cb4731cb9252c81e1505c04bfa923b7826
hervekhg2 <hervekouamo@gmail.com>
2019-03-16 23:20:52 +0100

    Créer son propre component et l'exporter/l'importer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a react App for Learning </h1>
+        <p>This working fine. I love it</p>
+        <Person/>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "This my React App"));
   }
 }
 
