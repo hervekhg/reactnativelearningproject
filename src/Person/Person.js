3baf702611b518d0e5b9aa8d96d1325634a2f719
hervekhg2 <hervekouamo@gmail.com>
2019-03-17 14:29:24 +0100

    12.Ajouter le CSS

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 // ES6 Syntax
 
 import React from 'react';
+import './Person.css';
 
 
 const person = (props) => {
 	return (
-		<div>
+		<div className="Person">
 			<p onClick= {props.click}> I am {props.name} a person and I am {props.age} Year old!</p>
 			<p> {props.children} </p>
 			<input type="text" onChange={props.changed}/>
